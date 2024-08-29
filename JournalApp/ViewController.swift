7026ff7b5cd92a7cb454aa841f6aa437d9bc235d
Navi9519 <dedicivanovic@gmail.com>
2024-08-29 09:25:19 +0200

    Initial Commit

---
+++ JournalApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JournalApp
+//
+//  Created by Ivan Dedic on 2024-08-29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
